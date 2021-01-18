3cb7b521d1717185d8a6cd16c55b55de3bc290e9
noman-khalid-dev <noman.khalid.freelance@gmail.com>
2021-01-19 00:49:44 +0500

    initial commit

---
+++ android/app/src/main/kotlin/noman/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package noman.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,//color of the scaffold widget
+      ),
+    ),
+  );//runApp
+}//main
